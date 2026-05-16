8d60d8242ae6134c8762f07386fa945f8c6892fd
AsmaAlghamdi1 <Asmaa.sa1357@gmail.com>
2026-05-16 15:04:17 +0300

    fix backend path

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,7 +1,8 @@
 const multer = require("multer");
+const path = require("path");
 
 const upload = multer({
-  dest: "uploads/",
+  dest: path.join(__dirname, "../uploads"),
 });
 
 module.exports = upload;
